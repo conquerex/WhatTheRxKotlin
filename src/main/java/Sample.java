f05be122b2a158465da0c34419efa2291a1f9960
Jongkook <conquerex@gmail.com>
2020-12-15 01:56:11 +0900

    chapter4. 플로어블 Flowables

---
+++ src/main/java/Sample.java
@@ -0,0 +1,8 @@
+/**
+ * @author Jongkook
+ * @date : 2020/12/13
+ */
+
+public class Sample {
+    //
+}
--- src/main/kotlin/Chapter4.kt
+++ src/main/kotlin/Chapter4.kt
@@ -1,3 +1,4 @@
+import io.reactivex.Flowable
 import io.reactivex.Observable
 import io.reactivex.schedulers.Schedulers
 import io.reactivex.subjects.BehaviorSubject
@@ -22,6 +23,7 @@ fun main(args: Array<String>) {
 
     val observable1 = Observable.just(1, 2, 3, 4, 5)
     val subject = BehaviorSubject.create<Int>()
+    // 다른 처리를 빨리 진햋위해 아래 수행을 주석처리
 //    subject.observeOn(Schedulers.computation())
 //        .subscribe {
 //            println("Subs 1 Received $it")
@@ -42,16 +44,81 @@ fun main(args: Array<String>) {
     observable1.map { MyItem(it) }
         .observeOn(Schedulers.computation())
         .subscribe {
-            println("Received $it")
-            runBlocking {
-                delay(200)
-            }
+//            println("Received $it")
+//            runBlocking {
+//                delay(200)
+//            }
         }
 
-    runBlocking {
-        delay(1500)
-    }
+    // 다른 처리를 빨리 진햋위해 아래 수행을 주석처리
+//    runBlocking {
+//        delay(1500)
+//    }
 
+    println(
+        """
+        
+        ******************************
+        플로어블 Flowables
+        ******************************
+        
+    """.trimIndent()
+    )
+
+//    Observable.range(1, 1000)
+//        .map { MyItem(it) }
+//        .observeOn(Schedulers.computation())
+//        .subscribe({
+//            print("Received $it\t")
+//            runBlocking {
+//                delay(50)
+//            }
+//        }, {
+//            it.printStackTrace()
+//        })
+//    runBlocking { delay(10000) }
+
+    Flowable.range(1, 1000)
+        .map { MyItem(it) }
+        .observeOn(Schedulers.io())
+        .subscribe({
+            print("Received $it\t")
+            runBlocking {
+                delay(50)
+            }
+        }, {
+            it.printStackTrace()
+        })
+    runBlocking { delay(5000) }
+
+
+    println(
+        """
+        
+        ******************************
+        
+        ******************************
+        
+    """.trimIndent()
+    )
+    println(
+        """
+        
+        ******************************
+        
+        ******************************
+        
+    """.trimIndent()
+    )
+    println(
+        """
+        
+        ******************************
+        
+        ******************************
+        
+    """.trimIndent()
+    )
     println(
         """
         
