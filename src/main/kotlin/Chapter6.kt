1df7187839f972fb8fd7d9963ebcdd5d638d29c4
Jongkook <conquerex@gmail.com>
2020-12-23 17:41:04 +0900

    chapter6. 배출을 집핑하기 : zip 연산자

--- src/main/kotlin/Chapter6.kt
+++ src/main/kotlin/Chapter6.kt
@@ -1,3 +1,7 @@
+import io.reactivex.Observable
+import io.reactivex.functions.BiFunction
+import io.reactivex.rxkotlin.toObservable
+
 /**
  * @author Jongkook
  * @date : 2020/12/23
@@ -9,12 +13,25 @@ fun main(args: Array<String>) {
         """
         
         ******************************
-        
+        startWith 연산자
         ******************************
         
     """.trimIndent()
     )
 
+    Observable.range(5, 10)
+        .startWith(listOf(1, 2, 3, 4))
+        .subscribe {
+            println("Rec $it")
+        }
+
+    println("startWith another source Producer")
+
+    Observable.range(5, 10)
+        .startWith(Observable.just(1, 2, 3, 4))
+        .subscribe {
+            println("Rec $it")
+        }
 
 
 
@@ -22,13 +39,23 @@ fun main(args: Array<String>) {
         """
         
         ******************************
-        
+        배출을 집핑하기 : zip 연산자
         ******************************
         
     """.trimIndent()
     )
 
+    val observable1 = Observable.range(1, 5)
+    val observable2 = Observable.range(11, 5)
+    Observable.zip(observable1, observable2, BiFunction { t1, t2 -> t1 + t2 })
+        .subscribe {
+            println("Rec $it")
+        }
 
+
+    val observable3 = listOf("aa", "bb", "c", "ddd", "e").toObservable()
+    observable1.zipWith(observable3, { e1: Int, e2: String -> "$e2 $e1" })
+        .subscribe { println("Rec $it") }
 
 
     println(
