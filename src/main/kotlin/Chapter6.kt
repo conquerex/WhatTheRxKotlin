8dc86735eb2039d8211d77c93a2936016ee346b8
Jongkook <conquerex@gmail.com>
2020-12-24 18:20:39 +0900

    chapter6. 프로듀서 이어 붙이기(옵저버블/플로어블)

--- src/main/kotlin/Chapter6.kt
+++ src/main/kotlin/Chapter6.kt
@@ -82,17 +82,58 @@ fun main(args: Array<String>) {
     val observable5 = Observable.interval(100, TimeUnit.MILLISECONDS)
     val observable6 = Observable.interval(250, TimeUnit.MILLISECONDS)
 
-    Observable.combineLatest(observable5, observable6, BiFunction { t1, t2 -> "$t1 $t2" })
+//    Observable.combineLatest(observable5, observable6, BiFunction { t1, t2 -> "$t1 $t2" })
+//        .subscribe { println("Rec $it") }
+//
+//    runBlocking { delay(1100) }
+
+
+    println(
+        """
+        
+        ******************************
+        옵저버블/플로어블 병합 : merge 연산자
+        ******************************
+        
+    """.trimIndent()
+    )
+
+    val observable7 = listOf("aa", "bbb", "c", "dd").toObservable()
+    val observable8 = listOf("qq", "wwww", "ee", "r").toObservable()
+
+    Observable.merge(observable7, observable8)
         .subscribe { println("Rec $it") }
 
-    runBlocking { delay(1100) }
+
+    val ob1 = Observable.interval(500, TimeUnit.MILLISECONDS)
+        .map { "Observable 1 $it" }
+    val ob2 = Observable.interval(100, TimeUnit.MILLISECONDS)
+        .map { "Observable 2 $it" }
+//    Observable.merge(ob1, ob2)
+//        .subscribe { println(">> Rec $it") }
+//    runBlocking { delay(1500) }
+
+    val test1 = listOf("a", "b", "c").toObservable()
+    val test2 = listOf("d", "e", "f", "g").toObservable()
+    val test3 = listOf("h", "i", "j", "k").toObservable()
+    val test4 = listOf("l", "m", "n", "o").toObservable()
+    val test5 = listOf("p", "q", "r", "s").toObservable()
+    val test6 = listOf("t", "u").toObservable()
+    Observable.mergeArray(test1, test2, test3, test4, test5, test6)
+        .subscribe { println("Rec $it") }
+
+    val ob3 = listOf("aa", "bb", "c").toObservable()
+    val ob4 = listOf("ddd", "e", "f").toObservable()
+    ob3.mergeWith(ob4).subscribe {
+        println(">> Rec $it")
+    }
 
 
     println(
         """
         
         ******************************
-        
+        프로듀서 이어 붙이기(옵저버블/플로어블)
         ******************************
         
     """.trimIndent()
@@ -101,42 +142,56 @@ fun main(args: Array<String>) {
 
 
 
-    println(
-        """
-        
-        ******************************
-        
-        ******************************
-        
-    """.trimIndent()
-    )
-
-
-
 
     println(
         """
         
         ******************************
-        
+        프로듀서 이어 붙이기(옵저버블/플로어블)
         ******************************
         
     """.trimIndent()
     )
 
+//    val ob5 = Observable.interval(500, TimeUnit.MILLISECONDS)
+//        .take(2)
+//        .map {
+//            "Observable 1 > $it"
+//        }
+//    val ob6 = Observable.interval(100, TimeUnit.MILLISECONDS)
+//        .map {
+//            "Observable 2 > $it"
+//        }
 
+//    Observable.concat(ob5, ob6)
+//        .subscribe {
+//            println("Rec $it")
+//        }
+//    runBlocking { delay(1500) }
 
 
     println(
         """
         
         ******************************
-        
+        프로듀서 임의 결합
         ******************************
         
     """.trimIndent()
     )
 
+    val ob5 = Observable.interval(500, TimeUnit.MILLISECONDS)
+        .map {
+            "Observable 1 > $it"
+        }
+    val ob6 = Observable.interval(100, TimeUnit.MILLISECONDS)
+        .map {
+            "Observable 2 > $it"
+        }
+
+    Observable.amb(listOf(ob5, ob6))
+        .subscribe { println("Rec $it") }
+    runBlocking { delay(1500) }
 
 
 
