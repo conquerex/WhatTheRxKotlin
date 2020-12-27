ec9b9a370f8ecd2dfc8ba22dcd23eebe27b66db7
Jongkook <conquerex@gmail.com>
2020-12-27 18:04:50 +0900

    chapter7. 스케줄러 종류

---
+++ src/main/kotlin/Chapter7.kt
@@ -0,0 +1,218 @@
+import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+
+/**
+ * @author Jongkook
+ * @date : 2020/12/27
+ */
+
+fun main(args: Array<String>) {
+
+    println(
+        """
+        
+        ******************************
+        스케줄러 종류
+        ******************************
+        
+    """.trimIndent()
+    )
+
+//    Observable.range(1,10)
+//        .subscribe {
+//            runBlocking { delay(200) }
+//            println("Obs1 Item Rec $it")
+//        }
+//
+//    Observable.range(21, 10)
+//        .subscribe {
+//            runBlocking { delay(100) }
+//            println("Obs2 Item Rec $it")
+//        }
+
+
+//    Observable.range(1,10)
+//        .subscribeOn(Schedulers.computation())
+//        .subscribe {
+//            runBlocking { delay(200) }
+//            println("Obs1 Item Rec $it")
+//        }
+//
+//    Observable.range(21, 10)
+//        .subscribeOn(Schedulers.computation())
+//        .subscribe {
+//            runBlocking { delay(100) }
+//            println("Obs2 Item Rec $it")
+//        }
+//
+//    runBlocking { delay(2100) }
+
+
+    println(
+        """
+        
+        ******************************
+        
+        ******************************
+        
+    """.trimIndent()
+    )
+
+
+
+    println(
+        """
+        
+        ******************************
+        
+        ******************************
+        
+    """.trimIndent()
+    )
+
+
+
+    println(
+        """
+        
+        ******************************
+        
+        ******************************
+        
+    """.trimIndent()
+    )
+
+
+
+    println(
+        """
+        
+        ******************************
+        
+        ******************************
+        
+    """.trimIndent()
+    )
+
+
+
+    println(
+        """
+        
+        ******************************
+        
+        ******************************
+        
+    """.trimIndent()
+    )
+
+
+
+    println(
+        """
+        
+        ******************************
+        
+        ******************************
+        
+    """.trimIndent()
+    )
+
+
+
+    println(
+        """
+        
+        ******************************
+        
+        ******************************
+        
+    """.trimIndent()
+    )
+
+
+
+    println(
+        """
+        
+        ******************************
+        
+        ******************************
+        
+    """.trimIndent()
+    )
+
+
+
+    println(
+        """
+        
+        ******************************
+        
+        ******************************
+        
+    """.trimIndent()
+    )
+
+
+
+    println(
+        """
+        
+        ******************************
+        
+        ******************************
+        
+    """.trimIndent()
+    )
+
+
+
+    println(
+        """
+        
+        ******************************
+        
+        ******************************
+        
+    """.trimIndent()
+    )
+
+
+
+    println(
+        """
+        
+        ******************************
+        
+        ******************************
+        
+    """.trimIndent()
+    )
+
+
+
+    println(
+        """
+        
+        ******************************
+        
+        ******************************
+        
+    """.trimIndent()
+    )
+
+
+
+    println(
+        """
+        
+        ******************************
+        
+        ******************************
+        
+    """.trimIndent()
+    )
+}
